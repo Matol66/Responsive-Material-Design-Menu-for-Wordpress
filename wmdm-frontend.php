dd7e3fb06b245abbe818c8efd2791b41a6e147c6
Matol66 <mateusz.lewandowski@ddspoland.com>
2015-05-14 11:01:35 +0200

    Rewrited plugin - new preRelease
    Rewrited to be Plugin API ready
    @make on classess
    @added logo upload function
    @ added help and comments

---
+++ wmdm-frontend.php
@@ -0,0 +1,85 @@
+
+<?php function print_frontend($options){ ?>
+<!--    --><?php //var_dump($options); ?>
+
+    <div class="container visible-xs header-mobile">
+
+    <div class="row depth-1 nav-bar">
+        <div class="col-xs-6">
+            <i class="icon-menu"><span class="menu-word">Menu</span></i>
+        </div>
+        <div class="col-xs-6" style="text-align:right;">
+            <a href="<?php echo esc_url( home_url( '/' ) )?>" title="<?php bloginfo('name'); ?>" class="mobile-go-home">
+                <img src="<?php echo esc_attr( $options['logo_image'] ); ?>" alt="<?php bloginfo('name'); ?>" id="mobile-header-logo">
+            </a>
+        </div>
+    </div>
+
+    <?php if( is_home() ) : ?>
+        <div class="q-action-wrapper row">
+            <?php $counter = 1; ?>
+            <?php while($counter<4) : ?>
+                <div class="col-xs-4 q-action">
+                    <div>
+                        <a href="<?php echo esc_attr( $options['box_'.$counter.'_link'] ); ?>">
+                            <i class="<?php echo esc_attr( $options['box_'.$counter.'_class'] ); ?>"></i>
+                            <span><?php echo esc_attr( $options['box_'.$counter.'_label'] ); ?></span>
+                        </a>
+                    </div>
+                </div>
+
+                <?php $counter++; endwhile; ?>
+        </div>
+    <?php endif; ?>
+</div>
+
+<div class="container-fluid visible-sm visible-md depth-1 header-mobile tablet" >
+    <div class="row" style="padding:10px 0">
+        <div class="col-sm-6">
+            <i class="icon-bs icon-menu"><span class="menu-word">Menu</span></i>
+        </div>
+        <div class="col-sm-6">
+            <?php if( $options['tablet-right-place'] == 'logo') : ?>
+                <a href="<?php echo esc_url( home_url( '/' ) )?>" title="<?php bloginfo('name'); ?>" class="mobile-go-home">
+                    <img src="<?php echo esc_attr( get_option('logo_path') ); ?>" alt="<?php bloginfo('name'); ?>" id="mobile-header-logo">
+                </a>
+            <?php elseif( $options['tablet-right-place'] == 'search') : ?>
+                <form role="search" method="get" id="searchform" class="searchform push-right" action="http://beta.bstczew.com/">
+                    <input type="text" value="" name="s" id="s" placeholder="szukaj"><i class="md icon-search"></i>
+                </form>
+            <?php else: ?>
+                <a href="http://beta.bstczew.com/strefa-logowania/" class="login-btn" style="float:right">
+                    <i class="icon-bs icon-lock"><span>Logowanie</span></i>
+                </a>
+            <?php endif; ?>
+        </div>
+    </div>
+
+</div>
+
+
+<div class="col-xs-10 col-sm-5 mobile-menu-toggle hidden-lg">
+    <div class="row toggle-header">
+        <div class="col-xs-9 phone-contact">
+            <?php if( $options['unfolded_1'] == 'phone') : ?>
+                <a href="<?php echo $options['login_phone']; ?>"><i class="icon-phone"></i>Skontaktuj się</a>
+            <?php elseif( $options['unfolded_1'] == 'find') : ?>
+                <form role="search" method="get" id="searchform" class="searchform" action="http://beta.bstczew.com/">
+                    <input type="text" value="" name="s" id="s" placeholder="szukaj"><i class="icon-bs icon-search_bs"></i>
+                </form>
+            <?php else : ?>
+                <i class="icon-lock"></i>Logowanie
+            <?php endif; ?>
+        </div>
+        <div class="col-xs-3 close-menu">
+            <i class="icon-close"></i>
+        </div>
+    </div>
+    <div class="row visible-xs">
+        <?php wp_nav_menu( array( 'theme_location' => 'mobile-menu-material' ) ); ?>
+    </div>
+    <div class="row visible-sm visible-md">
+        <?php wp_nav_menu( array( 'theme_location' => 'tablet-menu-material' ) ); ?>
+    </div>
+</div>
+<?php } ?>
--- wordpress-material-design-menu.php
+++ wordpress-material-design-menu.php
@@ -1,133 +1,256 @@
 <?php
 /*
 Plugin Name: WordPress Material Design Menu
-Description: Displays Menu - Tablet / Mobile - Bootstrap based
-Version: 0.1
+Description: Displays Menu - Tablet / Mobile - Bootstrap based. <hr><strong>IMPORTANT</strong> Libraries: Velocity(animations), jQuery(js handler), WMDM(plugin script) | Styles: Bootstrap(Grid), Icofont(Material Icons), WMDM(plugin menu styles)
+Version: 0.9
 */
 ?>
 
 <?php
 
-include_once('wmdm-settings.php');
+class WMDM
+{
+    function __construct() {
+
+        // Load text domain
+//        add_action( 'init', array( $this, 'load_localisation' ), 0 );
+
+        // Adding Plugin Menu
+        add_action( 'admin_menu', array( &$this, 'wmdm_menu' ) );
+
+        // Load our custom assets.
+        add_action( 'admin_enqueue_scripts', array( &$this, 'wmdm_assets' ) );
+
+        add_action( 'wp_head', array( &$this, 'enqueue_libs' ) );
+
+        // Register Settings
+        add_action( 'admin_init', array( &$this, 'wmdm_settings' ) );
 
 
-function cc_mime_types($mimes) {
-    $mimes['svg'] = 'image/svg+xml';
-    return $mimes;
+        // Add Two Menus - Mobile & Tablet
+        add_action( 'init', array( &$this, 'register_material_menus' ));
+
+        //  Add Favicon to website frontend
+        add_action( 'wp_head', array( &$this, 'print_on_frontend' ) );
+
+        // Add Favicon to website backend
+        //add_action( 'admin_head', array( &$this, 'dot_cfi_favicon_backend' ) );
+//        add_action( 'login_head', array( &$this, 'dot_cfi_favicon_backend' ) );
+
+
+    } // end constructor
+
+
+
+
+    /*--------------------------------------------*
+     * Admin Menu
+     *--------------------------------------------*/
+
+    function wmdm_menu()
+    {
+        $page_title = __('Material Design Menu', 'wmdm');
+        $menu_title = __('Material Design Menu', 'wmdm');
+        $capability = 'manage_options';
+        $menu_slug = 'wmdm-options';
+        $function = array(&$this, 'wmdm_menu_contents');
+        add_options_page($page_title, $menu_title, $capability, $menu_slug, $function);
+
+    }
+
+    /*--------------------------------------------*
+     * Load Necessary JavaScript Files
+     *--------------------------------------------*/
+
+    function wmdm_assets() {
+        if (isset($_GET['page']) && $_GET['page'] == 'wmdm-options') {
+
+            wp_enqueue_style( 'thickbox' ); // Stylesheet used by Thickbox
+            wp_enqueue_script( 'thickbox' );
+            wp_enqueue_script( 'media-upload' );
+
+            wp_register_script('wmdm_admin', plugins_url( '/js/dot_cfi_admin.js' , __FILE__ ), array( 'thickbox', 'media-upload' ));
+            wp_enqueue_script('wmdm_admin');
+
+            wp_enqueue_style( 'plugin-admin', plugin_dir_url(__FILE__).'styles/admin-styles.css' );
+        }
+
+    }
+
+
+    function enqueue_libs(){
+        wp_enqueue_style( 'bootstrap-base', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css' );
+        wp_enqueue_style( 'icofont-base', plugin_dir_url(__FILE__).'style.css' );
+        wp_enqueue_style( 'plugin-base', plugin_dir_url(__FILE__).'styles/styles.css' );
+        wp_enqueue_script( 'jQuery', 'http://code.jquery.com/jquery-2.1.4.min.js', array(), '1.0.0', true );
+        wp_enqueue_script( 'velocity', 'https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.2/velocity.min.js', array(), '1.0.0', true );
+        wp_enqueue_script( 'plugin-base', plugin_dir_url(__FILE__).'js/wmdm.js', array(), '1.0.0', true );
+    }
+
+
+
+    /*--------------------------------------------*
+     * Settings & Settings Page
+     * dot_cfi_menu_contents
+     *--------------------------------------------*/
+
+    public function wmdm_menu_contents()
+    {
+        ?>
+        <div class="wrap">
+            <!--<div id="icon-freshdesk-32" class="icon32"><br></div>-->
+            <div id="icon-options-general" class="icon32"><br></div>
+            <h2><?php _e('Material Design Menu', 'wmdm'); ?></h2>
+
+            <form method="post" action="options.php">
+                <?php //wp_nonce_field('update-options'); ?>
+                <?php settings_fields('wmdm_settings'); ?>
+                <?php do_settings_sections('wmdm_settings'); ?>
+                <p class="submit">
+                    <input name="Submit" type="submit" class="button-primary"
+                           value="<?php _e('Zapisz zmiany', 'wmdm'); ?>"/>
+                </p>
+            </form>
+            <div class="helper">
+                <?php echo '
+                    <img src="'.plugins_url( '/sprite/class.png' , __FILE__ ).'" alt="" class="img-responsive"/>
+                    <h3>Podgląd klas ikon:</h3> <a href="'.plugins_url( '/sprite/sprite.html' , __FILE__ ).'" target="_blank">zobacz podgląd</a>
+                    <h3>Pole <i>"link"</i></h3>
+                    <p>
+                    Linki powinny być zapisane w formacie:<br>
+                    <h4><strong>http://</strong>link-do-strony.pl</h4>
+                    Dodatkowe opcje to:<br>
+                    <strong>mailto:</strong> adres@domena.pl<br>
+                    <strong>tel:</strong> +48123456789 (z kierunkowym kraju)<br>
+                    <strong>gps:</strong> 54.3610873,18.6900271
+                    </p>
+                '; ?>
+            </div>
+        </div>
+
+    <?php
+    }
+
+    function wmdm_settings()
+    {
+        register_setting('wmdm_settings', 'wmdm_settings');
+        add_settings_section('boxes', 'Trzy boksy wyświetlane na telefonach', array(&$this, 'section_boxes'), 'wmdm_settings');
+        add_settings_section('unfolded', 'Ustawienia nagłówka menu po rozwinięciu', array(&$this, 'section_unfolded'), 'wmdm_settings');
+        add_settings_section('logo_login', 'Ustawienia Logo / Panel logowania / Numer telefonu', array(&$this, 'section_logo_login'), 'wmdm_settings');
+        add_settings_section('tablet_menu', 'Ustawienia menu na tablecie', array(&$this, 'section_tablet'), 'wmdm_settings');
+
+//        add_settings_field('box_1_image', 'Logo', array( &$this, 'section_box_1_image' ), 'wmdm_settings', 'boxes');
+
+        add_settings_field('box_1', 'Pierwsza Ikona', array( &$this, 'section_box_1' ), 'wmdm_settings', 'boxes');
+        add_settings_field('box_2', 'Druga Ikona', array( &$this, 'section_box_2' ), 'wmdm_settings', 'boxes');
+        add_settings_field('box_3', 'Trzecia Ikona', array( &$this, 'section_box_3' ), 'wmdm_settings', 'boxes');
+
+        add_settings_field('unfold_1', 'Pojawi się: ', array( &$this, 'section_unfold_1' ), 'wmdm_settings', 'unfolded');
+
+        add_settings_field('logo_login_1', 'Logo', array( &$this, 'section_logo_login_1' ), 'wmdm_settings', 'logo_login');
+
+        add_settings_field('tablet_menu_1', 'Prawa strona menu na tablecie', array( &$this, 'section_tablet_menu' ), 'wmdm_settings', 'tablet_menu');
+
+    }
+
+
+    function  section_tablet_menu(){
+        $options = get_option( 'wmdm_settings' );
+        ?>
+        <span class="radio">
+            <label for="tablet_radio_1"><input type="radio" id="tablet_radio_1" name="wmdm_settings[tablet-right-place]" value="logo" <?php checked('logo', $options['tablet-right-place']); ?>/>Logo</label>
+            <label for="tablet_radio_2"><input type="radio" id="tablet_radio_2" name="wmdm_settings[tablet-right-place]" value="login" <?php checked('login', $options['tablet-right-place']); ?>/>Link do logowania</label>
+            <label for="tablet_radio_3"><input type="radio" id="tablet_radio_3" name="wmdm_settings[tablet-right-place]" value="search" <?php checked('search', $options['tablet-right-place']); ?>/>Wyszukiwarka</label>
+        </span>
+        <?php
+    }
+
+    function section_logo_login_1() {
+        $options = get_option( 'wmdm_settings' );
+        ?>
+
+        <span class='upload'>
+            <img src='<?php echo esc_url( $options["logo_image"] ); ?>' class='preview-upload' /></label>
+            <label for="wmdm_settings[logo_image]"><input type='text' id='wmdm_settings[logo_image]' class='regular-text text-upload' name='wmdm_settings[logo_image]' value='<?php echo esc_url( $options["logo_image"] ); ?>'/>
+                <input type='button' class='button button-upload' value='Upload an image'/></br>
+        </span>
+        <span class="text">
+           <label for="wmdm_settings[login_link]">Link logowania <input type='text' id='wmdm_settings[login_link]' class='regular-text' name='wmdm_settings[login_link]' value='<?php echo esc_url($options["login_link"]); ?>'/></label><br>
+           <label for="wmdm_settings[login_phone]">Numer telefonu <input type='text' id='wmdm_settings[login_phone]' class='regular-text' name='wmdm_settings[login_phone]' value='<?php echo $options["login_phone"]; ?>'/></label>
+       </span>
+    <?php
+    }
+
+    function section_box_1() {
+        $options = get_option( 'wmdm_settings' );
+        ?>
+
+        <span class='text'>
+            <label for="wmdm_settings[box_1_class]">Klasa <input type='text' id='wmdm_settings[box_1_class]' class='regular-text' name='wmdm_settings[box_1_class]' value='<?php echo $options["box_1_class"]; ?>'/></label>
+            <label for="wmdm_settings[box_1_label]">Opis <input type='text' id='wmdm_settings[box_1_label]' class='regular-text' name='wmdm_settings[box_1_label]' value='<?php echo $options["box_1_label"]; ?>'/></label>
+            <label for="wmdm_settings[box_1_link]">Link <input type='text' id='wmdm_settings[box_1_link]' class='regular-text' name='wmdm_settings[box_1_link]' value='<?php echo $options["box_1_link"] ; ?>'/></label>
+        </span>
+    <?php
+    }
+    function section_box_2() {
+        $options = get_option( 'wmdm_settings' );
+        ?>
+        <span class='text'>
+            <label for="wmdm_settings[box_2_class]">Klasa <input type='text' id='wmdm_settings[box_2_class]' class='regular-text' name='wmdm_settings[box_2_class]' value='<?php echo $options["box_2_class"]; ?>'/></label>
+            <label for="wmdm_settings[box_2_label]">Opis <input type='text' id='wmdm_settings[box_2_label]' class='regular-text' name='wmdm_settings[box_2_label]' value='<?php echo $options["box_2_label"]; ?>'/></label>
+            <label for="wmdm_settings[box_2_link]">Link <input type='text' id='wmdm_settings[box_2_link]' class='regular-text' name='wmdm_settings[box_2_link]' value='<?php echo  $options["box_2_link"]; ?>'/></label>
+        </span>
+    <?php
+    }
+    function section_box_3() {
+        $options = get_option( 'wmdm_settings' );
+        ?>
+        <span class='text'>
+            <label for="wmdm_settings[box_3_class]">Klasa <input type='text' id='wmdm_settings[box_3_class]' class='regular-text' name='wmdm_settings[box_3_class]' value='<?php echo $options["box_3_class"]; ?>'/></label>
+            <label for="wmdm_settings[box_3_label]">Opis <input type='text' id='wmdm_settings[box_3_label]' class='regular-text' name='wmdm_settings[box_3_label]' value='<?php echo $options["box_3_label"]; ?>'/></label>
+            <label for="wmdm_settings[box_3_link]">Link <input type='text' id='wmdm_settings[box_3_link]' class='regular-text' name='wmdm_settings[box_3_link]' value='<?php echo  $options["box_3_link"] ; ?>'/></label>
+        </span>
+    <?php
+    }
+    function section_unfold_1() {
+        $options = get_option( 'wmdm_settings' );
+        ?>
+        <span class='radio'>
+            <label for="headtype_radio_1"><input type="radio" id="headtype_radio_1" name="wmdm_settings[unfolded_1]" value="find" <?php checked('find', $options['unfolded_1']); ?>/>Wyszukiwarka</label>
+            <label for="headtype_radio_2"><input type="radio" id="headtype_radio_2" name="wmdm_settings[unfolded_1]" value="login" <?php checked('login', $options['unfolded_1']); ?>/>Link do logowania</label>
+            <label for="headtype_radio_3"><input type="radio" id="headtype_radio_3" name="wmdm_settings[unfolded_1]" value="phone" <?php checked('phone', $options['unfolded_1']); ?>/>Skontaktuj się z nami</label>
+        </span>
+    <?php
+    }
+
+
+
+
+
+
+
+
+    function register_material_menus() {
+        register_nav_menu('tablet-menu-material',__( 'Tablet Material Menu' ));
+        register_nav_menu('mobile-menu-material',__( 'Mobile Material Menu' ));
+    }
+
+
+    function section_boxes() 	{
+        $options = get_option( 'wmdm_settings' );
+//        var_dump($options);
+
+    }
+
+    function section_unfolded(){}
+    function section_logo_login(){}
+    function section_tablet(){}
+
+    function print_on_frontend(){
+        $options = get_option('wmdm_settings');
+        include_once 'wmdm-frontend.php';
+        print_frontend($options);
+    }
+
 }
-add_filter('upload_mimes', 'cc_mime_types');
-
-
-function register_material_menus() {
-    register_nav_menu('tablet-menu-material',__( 'Tablet Material Menu' ));
-    register_nav_menu('mobile-menu-material',__( 'Mobile Material Menu' ));
-}
-add_action( 'init', 'register_material_menus' );
-
-function enqueue_libs(){
-    wp_enqueue_style( 'bootstrap-base', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css' );
-    wp_enqueue_style( 'icofont-base', plugin_dir_url(__FILE__).'style.css' );
-    wp_enqueue_style( 'plugin-base', plugin_dir_url(__FILE__).'styles/styles.css' );
-
-    wp_enqueue_script( 'jQuery', 'http://code.jquery.com/jquery-2.1.4.min.js', array(), '1.0.0', true );
-    wp_enqueue_script( 'velocity', 'https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.2/velocity.min.js', array(), '1.0.0', true );
-    wp_enqueue_script( 'plugin-base', plugin_dir_url(__FILE__).'js/wmdm.js', array(), '1.0.0', true );
-}
-add_action( 'wp_enqueue_scripts', 'enqueue_libs' );
-
-function display_menu(){ ?>
-
-    <div class="container visible-xs header-mobile">
-
-        <div class="row depth-1 nav-bar">
-            <div class="col-xs-6">
-                <i class="icon-menu"><span class="menu-word">Menu</span></i>
-            </div>
-            <div class="col-xs-6" style="text-align:right;">
-                <a href="<?php echo esc_url( home_url( '/' ) )?>" title="<?php bloginfo('name'); ?>" class="mobile-go-home">
-                    <img src="<?php echo esc_attr( get_option('logo_path') ); ?>" alt="<?php bloginfo('name'); ?>" id="mobile-header-logo">
-                </a>
-            </div>
-        </div>
-
-        <?php if( is_home() ) : ?>
-            <div class="q-action-wrapper row">
-            <?php $counter = 1; ?>
-            <?php while($counter<4) : ?>
-                <div class="col-xs-4 q-action">
-                    <div>
-                        <a href="<?php echo esc_attr( get_option('link'.$counter) ); ?>">
-                            <i class="icon-<?php echo esc_attr( get_option('ico'.$counter) ); ?>"></i>
-                            <span><?php echo esc_attr( get_option('label'.$counter) ); ?></span>
-                        </a>
-                    </div>
-                </div>
-
-            <?php $counter++; endwhile; ?>
-            </div>
-        <?php endif; ?>
-
-        <div class="col-xs-10 col-sm-5 mobile-menu-toggle">
-            <div class="row toggle-header">
-                <div class="col-xs-9 phone-contact">
-                    <?php if(get_option('headtype')['radio1'] == 'find') : ?>
-                        <form role="search" method="get" id="searchform" class="searchform" action="http://beta.bstczew.com/">
-                            <input type="text" value="" name="s" id="s" placeholder="szukaj..."><i class="icon-bs icon-search_bs"></i>
-                        </form>
-                    <?php else : ?>
-                        LOOGOWNAIE
-                    <?php endif; ?>
-                </div>
-                <div class="col-xs-3 close-menu">
-                    <i class="icon-close"></i>
-                </div>
-            </div>
-            <div class="row menu-wrapper">
-                <?php wp_nav_menu( array( 'theme_location' => 'mobile-menu-material' ) ); ?>
-            </div>
-        </div>
-    </div>
-
-    <div class="container-fluid visible-sm visible-md depth-1 header-mobile tablet" >
-        <div class="row" style="padding:10px 0">
-            <div class="col-sm-6">
-                <i class="icon-bs icon-menu"><span class="menu-word">Menu</span></i>
-            </div>
-            <div class="col-sm-6">
-                <?php if(get_option('tablet-right')['radio2'] == 'logo') : ?>
-                    <a href="<?php echo esc_url( home_url( '/' ) )?>" title="<?php bloginfo('name'); ?>" class="mobile-go-home">
-                        <img src="<?php echo esc_attr( get_option('logo_path') ); ?>" alt="<?php bloginfo('name'); ?>" id="mobile-header-logo">
-                    </a>
-                <?php else: ?>
-                    <a href="http://beta.bstczew.com/strefa-logowania/" class="login-btn" style="float:right">
-                        <i class="icon-bs icon-lock"><span>&nbsp;&nbsp;&nbsp;Logowanie</span></i>
-                    </a>
-                <?php endif; ?>
-            </div>
-        </div>
-
-        <div class="col-xs-10 col-sm-5 mobile-menu-toggle">
-            <div class="row toggle-header">
-                <div class="col-xs-9 phone-contact">
-                    <?php if(get_option('headtype')['radio1'] == 'find') : ?>
-                    <form role="search" method="get" id="searchform" class="searchform" action="http://beta.bstczew.com/">
-                        <input type="text" value="" name="s" id="s" placeholder="szukaj"><i class="icon-bs icon-search_bs"></i>
-                        <!--<input type="submit" id="searchsubmit" value="Szukaj">-->
-                    </form>
-                    <?php else : ?>
-                        <i class="icon-lock"></i>Logowanie
-                    <?php endif; ?>
-                </div>
-                <div class="col-xs-3 close-menu">
-                    <i class="icon-bs icon-close"></i>
-                </div>
-            </div>
-            <div class="row">
-                <?php wp_nav_menu( array( 'theme_location' => 'tablet-menu-material' ) ); ?>
-            </div>
-        </div>
-    </div>
-
-<?php }
-add_action('wp_head','display_menu');
-
+$wmdm = new WMDM(__FILE__);
